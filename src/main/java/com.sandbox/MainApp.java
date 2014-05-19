8071f5e096ecd9c7b8b0c412bd3fb803ce15daf3
jmiller <jmiller@citytechinc.com>
2014-05-19 15:43:10 -0500

    added some stuff and added .gitignore

---
+++ src/main/java/com.sandbox/MainApp.java
@@ -0,0 +1,11 @@
+package sandbox;
+
+/**
+ * @author CITYTECH, INC. 2012
+ */
+public class MainApp {
+
+    public static void main( String args[]){
+        System.out.println("Hello World");
+    }
+}
